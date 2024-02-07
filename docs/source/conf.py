d4fbd01f49619bfa919375b68d88f5951cd4a8aa
Casper Kristiansson <casper.kristiansson@yahoo.se>
2024-02-07 16:02:57 +0100

    Update configs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,15 +14,25 @@ release = '[1.0]'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx_rtd_theme',
+    'rst2pdf.pdfbuilder'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
+pdf_documents = [('index', u'rst2pdf', u'Software Engineering Fundamentals Group 24', u'Andreas Hammarstrand (aeha@kth.se), Martin Lindefors (lindefor@kth.se), Victor Österman (vios@kht.se), Adam Sjöberg (adamsjo@kth.se), Casper Kristiansson (casperkr@kth.se)')]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
+
+# -- Path setup --------------------------------------------------------------
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../../src'))
