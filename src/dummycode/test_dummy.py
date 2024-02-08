1bad67f8cd0bf64cce657319da27c91b14b0a27f
Victor Österman <victor199907@hotmail.com>
2024-02-08 17:04:24 +0100

    fix: path in test_dummy.py

--- src/dummycode/test_dummy.py
+++ src/dummycode/test_dummy.py
@@ -1,8 +1,8 @@
 import unittest
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
-from dummycode.dummy import Dummy as d
+
+from dummy import Dummy as d
 
 
 class TestDummy(unittest.TestCase):
