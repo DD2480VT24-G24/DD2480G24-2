611d3bad92f48754fe4fe4603275539d217d0b0f
Victor Österman <victor199907@hotmail.com>
2024-02-08 17:01:58 +0100

    add: dummy.py and test_dummy.py
    dummy.py: contains simple code with documentation
    
    test_dummy.py: contains tests to test dummy functions

---
+++ src/dummycode/dummy.py
@@ -0,0 +1,65 @@
+class Dummy:
+    def add(a,b):
+        """
+        Simple method that adds two parameters
+
+        Parameters:
+        - a: a number
+        - b: a number
+
+        Returns:
+        - a+b: The sum of a and b.
+        """
+        return a + b
+
+    def sub(a,b):
+        """
+        Simple method that subtracts two parameters
+
+        Parameters:
+        - a: a number
+        - b: a number
+
+        Returns:
+        - a-b: The sum of a and b.
+        """
+        return a - b
+
+    def mult(a,b):
+        """
+        Simple method that multiplies two parameters
+
+        Parameters:
+        - a: a number
+        - b: a number
+
+        Returns:
+        - a*b: The product sum of a and b.
+        """
+        return a*b
+
+    def div(a,b):
+        """
+        Simple method that divide two parameters
+
+        Parameters:
+        - a: a number
+        - b: a number
+
+        Returns:
+        - a/b: The division of a over b.
+        """
+        return a/b
+
+    def pow(a,b):
+        """
+        Simple method that take one parameter to the power of the other
+
+        Parameters:
+        - a: a number
+        - b: a number
+
+        Returns:
+        - a-b: The sum of a and b.
+        """
+        return a**b
