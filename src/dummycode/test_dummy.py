ad03f480377ecb3abea78b7d2cad3eb944a37067
Victor Österman <99597722+VictorOsterman@users.noreply.github.com>
2024-02-08 17:11:02 +0100

    Issue#13 dummy code (#25)
    * add: dummy.py and test_dummy.py
    
    dummy.py: contains simple code with documentation
    
    test_dummy.py: contains tests to test dummy functions
    
    * fix: path in test_dummy.py

---
+++ src/dummycode/test_dummy.py
@@ -0,0 +1,22 @@
+import unittest
+import sys
+import os
+
+from dummy import Dummy as d
+
+
+class TestDummy(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(d.add(4,5), 9)
+    
+    def test_sub(self):
+        self.assertEqual(d.sub(4,5), -1)
+
+    def test_mult(self):
+        self.assertEqual(d.mult(4,5), 20)
+
+    def test_div(self):
+        self.assertEqual(d.div(4,2), 2)
+    
+    def test_pow(self):
+        self.assertEqual(d.pow(2,10), 1024)
