d9559d30964155e52d0e5b89dfcad09cc579d870
Victor Österman <victor199907@hotmail.com>
2024-02-08 17:35:22 +0100

    doc: add documentation to _clone_repo method in utils.py

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,7 +1,19 @@
 import git
 
 def _clone_repo(repo_url, destination_path):
-    
+    """
+    Clones a Git repository from the specified URL to the given destination path.
+
+    Parameters:
+        repo_url (str): The URL of the Git repository to clone.
+        destination_path (str): The path where the repository will be cloned.
+
+    Returns:
+        None
+
+    Raises:
+        git.exc.GitCommandError: If an error occurs during the cloning process.
+    """
     try:
         git.Repo.clone_from(repo_url, destination_path)
         print("Repository cloned successfully.")
