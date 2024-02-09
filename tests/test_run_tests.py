30d9655bbf31ce33229d8d95de0c1901787a562c
Victor Österman <victor199907@hotmail.com>
2024-02-09 16:55:31 +0100

    fix: issue connected to import

--- tests/test_run_tests.py
+++ tests/test_run_tests.py
@@ -4,7 +4,7 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
-import tests.run_tests as run_tests
+import src.tests.run_tests as run_tests
 
 
 class TestRunTests(unittest.TestCase):
