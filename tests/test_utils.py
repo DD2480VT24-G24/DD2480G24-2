a805270e18ffcd5db6351934dcb626058aa7f9cc
Martin Lindefors <lindefor@kth.se>
2024-02-09 14:37:01 +0100

    add: tests for the webhook validation method

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,7 @@ import sys, os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
-from src.utils.utils import _clone_repo, _remove_repo
+from src.utils.utils import _clone_repo, _remove_repo, verify_webhook_signature
 
 class TestUtils(unittest.TestCase):
     """
@@ -16,3 +16,41 @@ class TestUtils(unittest.TestCase):
             self.assertTrue(len(contents) > 0)
         else:
             self.fail('Repo wasn\'t successfully cloned')
+
+    def test_verify_webhook_signature_correct(self):
+        """
+        Check that the webhook verification accepts correct signature by testing it on the example from the
+        GitHub documentation on "Validating webhook deliveries" under subsection 
+        "Testing the webhook payload validation"
+        """
+
+        secret = "It's a Secret to Everybody"
+        payload = "Hello, World!".encode()
+        signature = "sha256=757107ea0eb2509fc211221cce984b8a37570b6d7586c22c46f4379c8b043e17"
+
+        self.assertTrue(verify_webhook_signature(payload, secret, signature))
+
+    
+    def test_verify_webhook_signature_incorrect_payload(self):
+        """
+        Check that the webhook verification rejects correct signature by testing the same example
+        as in `test_verify_webhook_signature_correct` but with a modified payload
+        """
+
+        secret = "It's a Secret to Everybody"
+        payload = "Hello World!".encode()
+        signature = "sha256=757107ea0eb2509fc211221cce984b8a37570b6d7586c22c46f4379c8b043e17"
+
+        self.assertFalse(verify_webhook_signature(payload, secret, signature))
+
+    def test_verify_webhook_signature_incorrect_secret(self):
+        """
+        Check that the webhook verification rejects correct signature by testing the same example
+        as in `test_verify_webhook_signature_correct` but with a modified secret
+        """
+
+        secret = "It's not a Secret to Everybody"
+        payload = "Hello, World!".encode()
+        signature = "sha256=757107ea0eb2509fc211221cce984b8a37570b6d7586c22c46f4379c8b043e17"
+
+        self.assertFalse(verify_webhook_signature(payload, secret, signature))
