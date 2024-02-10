8d882b6b8190e819ce0e066ee54774c23294bf5f
Martin Lindefors <lindefor@kth.se>
2024-02-10 15:59:55 +0100

    Added status notification method

--- src/build/run_build.py
+++ src/build/run_build.py
@@ -1,3 +1,28 @@
+import requests
+import os
+from payload import Payload
+
+
+def set_status(commit_sha, state, description, target_url, repo_name, repo_owner, github_token):
+    #DOCS"#######
+
+    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/statuses/{commit_sha}"
+
+    headers = {
+        "Authorization": f"token {github_token}",
+        "Accept": "application/vnd.github+json",
+    }
+    data = {
+        "state": state,
+        #"target_url": target_url,  Add target URL here when it has been implemented
+        "description": description
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    response.raise_for_status()
+    return response.json()
+
+
 def build_application():
     """
     Execute the build command and creates a new GitHub release with the build artifacts.
