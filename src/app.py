30d6fca0f41f1a3da92a80c6fc75dc8d8b9d87d4
Casper Kristiansson <casper.kristiansson@yahoo.se>
2024-02-13 00:16:06 +0100

    Fix wrong function for endpoint

--- src/app.py
+++ src/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from logs.log_individual import get_log_individual
+from logs.logs_all import get_log_ids
 from build.run_build import build_application, test_results
 from syntax.run_syntax import syntax_checker
 from utils.run_tests import run_tests
@@ -8,8 +10,9 @@ app = Flask(__name__)
 app.add_url_rule('/build', 'build', build_application, methods=['POST'])
 app.add_url_rule('/output', 'build', test_results, methods=['GET'])
 app.add_url_rule('/test', 'test', run_tests, methods=['POST'])
-app.add_url_rule('/logs/all', 'logs_all', run_tests, methods=['GET'])
+app.add_url_rule('/logs/all', 'logs_all', get_log_ids, methods=['GET'])
 app.add_url_rule('/logs/<string:id>', 'logs_id', get_log_individual, methods=['GET'])
+app.add_url_rule('/syntax', 'syntax', syntax_checker, methods=['POST'])
 
 
 if __name__ == "__main__":
