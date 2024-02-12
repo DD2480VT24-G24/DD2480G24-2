417dafb72f30c06c9e2b3be59fd8a8a2e577342e
Victor Österman <99597722+VictorOsterman@users.noreply.github.com>
2024-02-12 16:33:08 +0100

    Issue#9 static syntax checkers (#40)
    * add: pyright version in requirement.txt
    
    * fix: import related to issue with git.exc.GitCommandError
    
    * fix: issues related to dummy.py and test_dummy.py
    
    * fix: issue connected to import
    
    * refactor: move run_test.py to utils to resolve issue where import from tests occurred
    
    * add: functionality to run pyright on .py files in src/ and tests/ dir
    
    * remove: main in run_syntax file
    
    * fix: run_syntax file in a manner that it can be handled correctly by run_build
    
    * move: run_syntax to utils directory
    
    * remove: pyrightconfig.json file
    
    Since a path towards tmp (which can be accessed from any directory), the pyrightconfig file isn't required any more
    
    * add: test for syntax_checker
    
    first test: check if pyright doesn't find any error when it shouldn't
    
    second test: check if pyright find an error when it should (missing + between a and b)
    
    closes #9

--- src/dummycode/test_dummy.py
+++ src/dummycode/test_dummy.py
@@ -1,22 +1,19 @@
 import unittest
-import sys
-import os
-
-from dummy import Dummy as d
+from dummy import add, sub, mult, div, pow
 
 
 class TestDummy(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(d.add(4,5), 9)
+        self.assertEqual(add(4,5), 9)
     
     def test_sub(self):
-        self.assertEqual(d.sub(4,5), -1)
+        self.assertEqual(sub(4,5), -1)
 
     def test_mult(self):
-        self.assertEqual(d.mult(4,5), 20)
+        self.assertEqual(mult(4,5), 20)
 
     def test_div(self):
-        self.assertEqual(d.div(4,2), 2)
+        self.assertEqual(div(4,2), 2)
     
     def test_pow(self):
-        self.assertEqual(d.pow(2,10), 1024)
+        self.assertEqual(pow(2,10), 1024)
--- src/utils/run_syntax.py
+++ src/utils/run_syntax.py
@@ -1,4 +1,6 @@
-def syntax_checker():
+import subprocess
+
+def syntax_checker(repo_path):
     """
     Executes a syntax check and updates the GitHub notification based on the test results.
 
@@ -6,7 +8,10 @@ def syntax_checker():
     with changes from all pull requests. It runs the tests and updates the GitHub notification
     to indicate whether the application has passed the syntax check.
 
-    :return: A tuple containing a message and a status code.
+    :return: A tuple containing a status code and a message
     :rtype: tuple
     """
-    return "Syntax check executed", 200
+    
+    results = subprocess.run(['pyright', f'{repo_path}/src/', f'{repo_path}/tests/'], capture_output=True, text=True)
+    return results.returncode, results.stdout
+    
