4528862581a0278acc74afc402513c3e1f0b067a
Casper Kristiansson <casper.kristiansson@yahoo.se>
2024-02-13 00:13:12 +0100

    Add get log individual endpoint

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.add_url_rule('/build', 'build', build_application, methods=['POST'])
 app.add_url_rule('/output', 'build', test_results, methods=['GET'])
 app.add_url_rule('/test', 'test', run_tests, methods=['POST'])
-app.add_url_rule('/syntax', 'syntax', syntax_checker, methods=['POST'])
+app.add_url_rule('/logs/all', 'logs_all', run_tests, methods=['GET'])
 
 
 if __name__ == "__main__":
